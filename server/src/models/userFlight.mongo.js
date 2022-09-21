3663fefed45d47f42bdef932319a6120ce651585
unknown <alandreid@hotmail.co.uk>
2022-09-21 11:57:45 +0100

    removed name

--- server/src/models/userFlight.mongo.js
+++ server/src/models/userFlight.mongo.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const flightSchema = new mongoose.Schema({
   user: {
-    name: {
-      type: String,
-      required: true,
-    },
+    // name: {
+    //   type: String,
+    //   required: true,
+    // },
     email: {
       type: String,
     },
