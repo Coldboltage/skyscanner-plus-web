7ff42e0fa24171c9e090cb9324669ae723aa8422
coldboltage <alandreid@hotmail.co.uk>
2023-01-17 11:05:25 +0000

    added config module

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { UserFlightsModule } from './user-flights/user-flights.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://coldbolt-flightscanner:vtwmT7B4HkObSyYc@scannerplus.zgsmw.mongodb.net/scannerplus?retryWrites=true&w=majority',
+    ),
+    UserFlightsModule,
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
