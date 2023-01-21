ad982176055f5b3dc265b896240fa756dc90de0f
coldboltage <alandreid@hotmail.co.uk>
2023-01-21 12:45:09 +0000

    ✨ user can be created via endpoint

--- nest-server/src/user/user.service.ts
+++ nest-server/src/user/user.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { BadRequestException } from '@nestjs/common/exceptions';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/user-flights/entities/user-flight.entity';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
   async create(createUserDto: CreateUserDto) {
-    console.log(createUserDto);
     if (
       !createUserDto.hasOwnProperty('email') &&
       !createUserDto.hasOwnProperty('sub')
     )
       throw new BadRequestException('sub_or_email_needed');
-
+    await this.userRepository.save(createUserDto);
     return 'This action adds a new user';
   }
 
