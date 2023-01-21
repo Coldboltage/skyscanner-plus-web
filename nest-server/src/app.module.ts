67506039741807cd49f31c04f6e62cc48929ff5f
coldboltage <alandreid@hotmail.co.uk>
2023-01-21 11:43:08 +0000

    🗃️ Postgres added

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { UserFlightsModule } from './user-flights/user-flights.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -14,6 +15,16 @@ import { ConfigModule } from '@nestjs/config';
     ),
     UserFlightsModule,
     ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'random1234',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
